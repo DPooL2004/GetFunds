267d61b2082940b90bf38923818325623e77cff5
DPooL2004 <dsrp002@gmail.com>
2024-04-22 19:02:24 -0500

    Creacion del repositorio

---
+++ android/app/src/main/kotlin/com/example/getfunds/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.getfunds
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GetFunds());
+}
+
+class GetFunds extends StatelessWidget {
+  const GetFunds({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
