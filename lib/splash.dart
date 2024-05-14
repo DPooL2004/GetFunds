a705d971c8a4d976802f75c9ab0c21dba3daa9e5
DPooL2004 <dsrp002@gmail.com>
2024-05-14 12:11:57 -0500

    Actualizo ruta de Splash

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:getfunds/formularios/register.dart';
+import 'package:getfunds/vistas/bienvenida.dart';
 import 'package:lottie/lottie.dart';
 
 class Splash extends StatelessWidget {
@@ -8,26 +9,24 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width*1,
+        height: MediaQuery.of(context).size.height*1,
+        decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage('img/texturaSplash.jpg'),
-              fit: BoxFit.cover
+                image: AssetImage('img/texturaSplash.jpg',),
+                fit: BoxFit.cover
             )
-          ),
         ),
-        Positioned(
-          child: AnimatedSplashScreen(
-              splash: Lottie.asset('img/GetFundsGif.json'),
-              nextScreen: Register(),
-            duration: 2400,
-            splashIconSize: 600,
-            splashTransition: SplashTransition.fadeTransition,
-          ),
+        child: AnimatedSplashScreen(
+          splash: Lottie.asset('img/GetFundsGif.json'),
+          nextScreen: Bienvenida(),
+          duration: 2400,
+          splashIconSize: 600,
+          splashTransition: SplashTransition.fadeTransition,
         ),
-      ],
+      ),
     );
   }
 }
