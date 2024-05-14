b4371f40141d444aaaf310b6cd6477c8b20b023a
DPooL2004 <dsrp002@gmail.com>
2024-05-14 12:10:37 -0500

    Creo formularios de Registro e Inicio de Sesión

---
+++ lib/formularios/login.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+
+  final Color colorPrincipal = Color.fromRGBO(17,152,34,1);
+  final Color colorSecundario = Color.fromRGBO(42,114,33,1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width*1,
+        height: MediaQuery.of(context).size.height*1,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('img/Fondo_Verde.jpg'),
+                fit: BoxFit.cover
+            )
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25, right: 25, top: 40, bottom: 40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text('Inicia Sesión',
+                          style: TextStyle(
+                              color: colorPrincipal,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width*1,
+                          child: TextField(
+                            cursorColor: colorSecundario,
+                            decoration: InputDecoration(
+                              labelText: 'Correo',
+                              labelStyle: TextStyle(color: colorSecundario),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: colorSecundario),
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius:BorderRadius.circular(50),
+                                  borderSide: BorderSide(color: colorSecundario)
+                              ),
+
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width*1,
+                          child: TextField(
+                            cursorColor: colorSecundario,
+                            decoration: InputDecoration(
+                              labelText: 'Contraseña',
+                              labelStyle: TextStyle(color: colorSecundario),
+                              focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: colorSecundario),
+                                  borderRadius: BorderRadius.circular(50)
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius:BorderRadius.circular(50),
+                                  borderSide: BorderSide(color: colorSecundario)
+                              ),
+
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        FilledButton(
+                            style: FilledButton.styleFrom(
+                                backgroundColor: Colors.transparent
+                            ),
+                            onPressed: (){},
+                            child: Text("¿Olvidaste tu Contraseña?",
+                              style: TextStyle(
+                                  color: colorPrincipal,
+                                  fontFamily: 'Jost',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 15
+                              ),
+                            )
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width*1,
+                          height: 50,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: colorPrincipal,
+                                elevation: 3
+                            ),
+                            onPressed: (){},
+                            child: Text("Iniciar Sesión",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Jost'
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FilledButton(
+                          style: FilledButton.styleFrom(
+                            backgroundColor: Colors.transparent
+                          ),
+                            onPressed: (){},
+                            child: Text("¿No tienes una cuenta?",
+                              style: TextStyle(
+                                  color: colorPrincipal,
+                                  fontFamily: 'Jost',
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17
+                              ),
+                            )
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
