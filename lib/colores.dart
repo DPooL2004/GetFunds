32c204ec15bb287c2ffc7e2cd007dfa89a15b490
DPooL2004 <dsrp002@gmail.com>
2024-05-22 19:52:45 -0500

    Añado nuevo color

--- lib/colores.dart
+++ lib/colores.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 const Color colorPrincipal = Color.fromRGBO(17,152,34,1);
 const Color colorSecundario = Color.fromRGBO(42,114,33,1);
+const Color colorGris = Color.fromRGBO(118,118,118,1);
